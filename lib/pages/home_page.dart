d43de637aa5604279709c7cac42fbaa3a0bad524
wasim Haider <wasimhaider14379@gmail.com>
2022-08-18 21:39:13 -0700

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+
 import 'package:flutter_application_1/models/catalog.dart';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 // DAY 11 WE learn about context and constrains
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+   final  catalogJson =  await rootBundle.loadString("assets/files/catalog.json");
+   final decodeData = jsonDecode(catalogJson);
+  var productsData = decodeData["products"];
+  print(productsData);
+  }
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
@@ -20,10 +40,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-            itemCount: dummyList .length,
+            itemCount: dummyList.length,
             itemBuilder: (context, index) {
               return ItemWidget(
-                item: dummyList [index],
+                item: dummyList[index],
               );
             }),
       ),
